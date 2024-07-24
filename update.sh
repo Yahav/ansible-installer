d0e92376006ec69e5bce8b4a976e221de52c7c7a
Yahav <yahav.shasha@gmail.com>
2024-07-24 20:42:28 +0300

    update script fix

--- update.sh
+++ update.sh
@@ -46,6 +46,7 @@ sudo dnf install -y ansible python3-pip python3-mysqlclient
 APP_ENV="${APP_ENV:-production}"
 UPDATE_REVISION="${UPDATE_REVISION:-1}"
 
+echo "Updating AzuraCast (Environment: $APP_ENV, Update revision: $UPDATE_REVISION)"
 
 if [[ ${APP_ENV} == "production" ]]; then
   if [[ -d ".git" ]]; then
